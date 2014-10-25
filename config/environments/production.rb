f2c2ffcae9353d851ac9f9e3a77674714b60d16a
CaptainChemist <stephen.jensen3@gmail.com>
2014-10-25 15:35:26 -0500

    uncommented production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #fix!
-  #  config.action_mailer.default_url_options = { host: 'http://omrails-scj.herokuapp.com/'}
+  config.action_mailer.default_url_options = { host: 'http://omrails-scj.herokuapp.com/'}
 
 end
