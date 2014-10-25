0985ce9207239ed934041be30fab4410a382896a
CaptainChemist <stephen.jensen3@gmail.com>
2014-10-25 18:50:27 -0500

    atlassian suggestion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,6 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #fix!
-  config.action_mailer.default_url_options = { host: 'omrails-scj.herokuapp.com', port: 3000}
+  config.action_mailer.default_url_options = { :host => 'omrails-scj.herokuapp.com:3000'}
 
 end
