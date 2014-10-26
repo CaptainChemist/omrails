30455a2aa2c184cb8a94afa413cfaa037ccbeaaf
CaptainChemist <stephen.jensen3@gmail.com>
2014-10-26 10:16:04 -0500

    added pins

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+  resources :pins
+
   root 'pages#home'
   get 'about' => 'pages#about'
+  #get 'pins' => 'pins#index'
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141025151446) do
+ActiveRecord::Schema.define(version: 20141026140317) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
